76c7f360d337ecdf83b2aa54a5e2efbfb176f0af
root <kc@alx.com>
2022-09-08 15:15:47 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
+/**
+ * File:    4-puts.c
+ * Author:  KC EMENIKE
+ **/
 #include <stdio.h>
 /**
  * main:    Prints "Programming is like building a multilingual
  *          puzzle, followed by a new line.
- *
- * Return:  Always 0.
- */
+ * Return:  0.
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
