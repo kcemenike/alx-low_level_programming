fc4e9a6f71c97e0b30c4111ca89cbf76ce8a24a9
root <kc@alx.com>
2022-09-08 15:41:22 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
 #include <stdio.h>
 
 /**
- * main - This is a description
-*
-* Description: Really don't care
-* Return: 0
-*/
+ * main() - function that can only be called
+ *
+ * Return:
+ * 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
