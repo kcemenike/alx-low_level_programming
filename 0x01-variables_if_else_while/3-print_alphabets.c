f511d93450c6bb4b85fd01d585f988bd492900e6
KC <kcemenike@github.com>
2022-09-15 23:35:33 -0300

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - program execution begins here
+ *
+ *Description
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
